227836e77f5775ea4a235b7e17ebcdcc323dfdf7
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-02 22:59:55 +0700

    Блок ввода в задаче 5.3.2 - готов!

--- Home_work_5.3/Program.cs
+++ Home_work_5.3/Program.cs
@@ -6,7 +6,18 @@ namespace Home_work_5._3
     {   // Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
         static void Main(string[] args)
         {
-
+            int [] x = new int[2];
+            int [] y = new int[2];
+            int [] z = new int[2];
+            for(int i=0; i<=1; i++)
+            { 
+                Console.Write($"Введите координату точки X{i}: ");
+                x[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Введите координату точки Y{i}: ");
+                y[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Write($"Введите координату точки Z{i}: ");
+                z[i] = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
