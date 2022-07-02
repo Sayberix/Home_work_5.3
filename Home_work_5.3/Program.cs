32c61966ba42dc95be51b78eecbfea929c1cd24c
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-02 21:22:53 +0700

    Начало работы над заданием 5.3.2

--- Home_work_5.3/Program.cs
+++ Home_work_5.3/Program.cs
@@ -3,16 +3,10 @@
 namespace Home_work_5._3
 {
     class Program
-    {   // Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
+    {   // Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
         static void Main(string[] args)
         {
-            Console.Write("Введите пятизначное число: ");
-            string number = Console.ReadLine();
-            //Console.WriteLine($"Массив из string равен: {number[1]}");
-            if(number[0] == number[4] && number[1] == number[3])
-                Console.WriteLine($"Введенное число {number} - являеся палиндромом!");
-            else
-                Console.WriteLine($"Введенное число {number} - НЕ являеся палиндромом!");
+
         }
     }
 }
