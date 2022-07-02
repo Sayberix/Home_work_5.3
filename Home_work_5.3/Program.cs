88165e803677f17de713f0cfaf9545beb31c8cf2
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-02 20:11:48 +0700

    Создание проекта для дз 5.3

---
+++ Home_work_5.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Home_work_5._3
+{
+    class Program
+    {   // Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
