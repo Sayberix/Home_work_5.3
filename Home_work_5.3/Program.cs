76bb04e43be4efc9670d89a0624016036b87fe02
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-02 20:28:01 +0700

    Блок ввода данных выполнен

--- Home_work_5.3/Program.cs
+++ Home_work_5.3/Program.cs
@@ -6,7 +6,14 @@ namespace Home_work_5._3
     {   // Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. 
         static void Main(string[] args)
         {
-            
+            int [] arr = new int[5];
+            for(int i = 0; i < 5; i++)
+            {
+                Console.Write($"Введите элемент массива под номером: {i} ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
         }
     }
 }
