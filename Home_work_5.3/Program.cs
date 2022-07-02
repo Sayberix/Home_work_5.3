bf20742ed717f839985fe1ef967977d1f0586009
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-02 21:18:56 +0700

    Задание 5.3.1 - выполнено!

--- Home_work_5.3/Program.cs
+++ Home_work_5.3/Program.cs
@@ -8,8 +8,11 @@ namespace Home_work_5._3
         {
             Console.Write("Введите пятизначное число: ");
             string number = Console.ReadLine();
-            
-
+            //Console.WriteLine($"Массив из string равен: {number[1]}");
+            if(number[0] == number[4] && number[1] == number[3])
+                Console.WriteLine($"Введенное число {number} - являеся палиндромом!");
+            else
+                Console.WriteLine($"Введенное число {number} - НЕ являеся палиндромом!");
         }
     }
 }
