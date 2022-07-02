3c7b2c9c4265142268acd9de2561a98da1affa62
Maxim Konev <sayberix.ru@yandex.ru>
2022-07-03 00:04:09 +0700

    Начало работы над заданием 5.3.3

--- Home_work_5.3/Program.cs
+++ Home_work_5.3/Program.cs
@@ -3,23 +3,10 @@
 namespace Home_work_5._3
 {
     class Program
-    {   // Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+    {   // Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
         static void Main(string[] args)
         {
-            int [] x = new int[2];
-            int [] y = new int[2];
-            int [] z = new int[2];
-            for(int i=0; i<=1; i++)
-            { 
-                Console.Write($"Введите координату точки X{i}: ");
-                x[i] = Convert.ToInt32(Console.ReadLine());
-                Console.Write($"Введите координату точки Y{i}: ");
-                y[i] = Convert.ToInt32(Console.ReadLine());
-                Console.Write($"Введите координату точки Z{i}: ");
-                z[i] = Convert.ToInt32(Console.ReadLine());
-            }
-            double distance = Math.Round(Math.Sqrt(Math.Pow(x[1]-x[0],2) + Math.Pow(y[1]-y[0],2) + Math.Pow(z[1]-z[0],2)),2);
-            Console.WriteLine($"Расстояние между точками: {distance}");
+
         }
     }
 }
